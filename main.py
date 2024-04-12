499016d940326d1c62c1983c83da553c02edbfc3
saizdx2010 <saiprashanth02012001@gmail.com>
2024-04-12 17:50:22 +0700

    finished reading book from the books directory

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('hello world from bookbot')
+def get_book_text(path):
+    with open(path) as file:
+        file_content = file.read()
+        return file_content
 
+
+def main():
+    output = get_book_text('./books/frankenstein.txt')
+    print(output)
+
+
+main()
