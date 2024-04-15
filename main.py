e44a20f681d6cc0a1684f94128082ca8fdd08262
saizdx2010 <saiprashanth02012001@gmail.com>
2024-04-15 09:56:30 +0700

    Counted every instance of characters that are in the book

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ def get_book_text(path):
         return file_content
 
 
+def word_count(text):
+    word_count_list = text.split()
+    return len(word_count_list)
+
+
 def main():
     output = get_book_text('./books/frankenstein.txt')
-    print(output)
+    lowered_chars = output.lower()
+    chars_dict = {}
+    for char in lowered_chars:
+        if char in chars_dict and char.isalnum:
+            chars_dict[char] += 1
+        elif char.isalnum:
+            chars_dict[char] = 1
+    # book_word_count = word_count(output)
+    print(chars_dict)
+    # print(f'{book_word_count} words found in the document')
 
 
 main()
